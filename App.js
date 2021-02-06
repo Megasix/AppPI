6aacf531de64b068bc1e966ab49f3753d7adb384
Megasix <75232015+Megasix@users.noreply.github.com>
2021-02-06 17:54:51 -0500

    a

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ Amplify.configure(config)
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to z working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
