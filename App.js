6cd13af5f96921bf7c6f770d985dff00e6fe02ff
Megasix <75232015+Megasix@users.noreply.github.com>
2021-02-06 22:26:22 -0500

    Ajout de la carte Google

--- App.js
+++ App.js
@@ -3,17 +3,25 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Amplify from 'aws-amplify'
 import config from './src/aws-exports'
+import MapView from 'react-native-maps'
+
 
 Amplify.configure(config)
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>test !</Text>
+      <Text>Maps : </Text>
+      <MapView>
+      style={StyleSheet.absoluteFillObject}
+      provider={MapView.PROVIDER_GOOGLE}
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
